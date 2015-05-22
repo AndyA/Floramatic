66343d5e5d63ff1f38c9d17561be092b3498ab4e
Andy Armstrong <andy@hexten.net>
2015-05-22 12:23:44 +0100

    Fix scaled pan.

--- www/js/floramatic.js
+++ www/js/floramatic.js
@@ -99,13 +99,17 @@ $(function() {
       $source.mousemove(function(e) {
         var x = e.pageX - $(e.target).offset().left;
         var y = e.pageY - $(e.target).offset().top;
-        var nx = init_zoom.x + (x - init_x);
-        var ny = init_zoom.y + (y - init_y);
-        if (Modifiers.down('shift')) {
-          nx = quantiser.quantiseDistance(nx);
-          ny = quantiser.quantiseDistance(ny);
+        if (1) {
+          zoom.setOffset(init_zoom.x + (x - init_x) / init_zoom.scale, init_zoom.y + (y - init_y) / init_zoom.scale);
+        } else {
+          var nx = init_zoom.x + (x - init_x);
+          var ny = init_zoom.y + (y - init_y);
+          if (Modifiers.down('shift')) {
+            nx = quantiser.quantiseDistance(nx);
+            ny = quantiser.quantiseDistance(ny);
+          }
+          zoom.setOffset(nx / init_zoom.scale, ny / init_zoom.scale);
         }
-        zoom.setOffset(nx / init_zoom.scale, ny / init_zoom.scale);
         controls.redraw();
       });
 
