fbeb8859978317c9e81c89514611a47f781fb7bf
Andy Armstrong <andy@hexten.net>
2015-05-25 19:50:05 +0100

    Use nice manifest builder.

--- tools/serve-me.js
+++ tools/serve-me.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var bodyParser = require('body-parser')
 var app = express();
-var fs = require('fs');
+var manifest = require('../lib/manifest.js');
 
 var WEBROOT = 'www';
 
@@ -21,98 +21,10 @@ app.use(function(err, req, res, next) {
   });
 });
 
-function findDirs(objs, done) {
-  var dirs = [];
-
-  function checkObj() {
-    if (!objs) return;
-    if (!objs.length) {
-      done(null, dirs);
-      return;
-    }
-
-    var obj = objs.shift();
-    fs.stat(obj, function(err, st) {
-      if (err) {
-        done(err, []);
-      }
-      else {
-        if (st.isDirectory()) {
-          dirs.push(obj);
-        }
-        checkObj();
-      }
-    });
-  }
-
-  checkObj();
-}
-
-function find(dir, like, done) {
-  var queue = [dir];
-  var found = [];
-
-  function scan() {
-    if (!queue) return;
-    if (!queue.length) {
-      done(null, found);
-      return;
-    }
-
-    var d = queue.shift();
-
-    fs.readdir(d, function(err, files) {
-      if (err) {
-        done(err, null);
-        return;
-      }
-
-      var subs = [];
-      for (var i = 0; i < files.length; i++) {
-        (like.test(files[i]) ? found : subs).push(d + '/' + files[i]);
-      }
-
-      findDirs(subs, function(err, dirs) {
-        if (err) {
-          done(err, null);
-          queue = null;
-        }
-        else {
-          Array.prototype.push.apply(queue, dirs);
-        }
-        scan();
-      });
-
-    });
-
-  }
-
-  scan();
-}
-
-var manifest = null;
-
 app.get('/art/manifest.json', function(req, res, next) {
-  if (manifest) {
-    res.send(manifest);
-    return
-  }
-
-  find(artroot, /\.(?:png|jpeg|jpg)$/i, function(err, files) {
-    if (err) {
-      next(err);
-      return;
-    }
-
-    manifest = {};
-    console.log("Found " + files.length + " images at " + artroot);
-    for (var i = 0; i < files.length; i++) {
-      var id = 'img' + i;
-      manifest[id] = '/art' + files[i].substr(artroot.length);
-    }
-    res.send(manifest);
+  manifest(artroot, /\.(?:png|jpeg|jpg)$/i, '/art').then(function(mani) {
+    res.send(mani);
   });
-
 });
 
 app.post('/rconsole/send', function(req, res) {
