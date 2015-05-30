3b200e84babfffc978a1a5e09e942ea20702bd4e
Andy Armstrong <andy@hexten.net>
2015-05-30 20:47:56 +0100

    Include canvas.

--- test/file-find.js
+++ test/file-find.js
@@ -34,7 +34,6 @@ describe('file-find', function() {
     var fs_root;
 
     before(function(done_it) {
-
       tempfs({
         'a': 'This is a',
         'b': 'This is b',
@@ -52,25 +51,17 @@ describe('file-find', function() {
     });
 
     it('should scan a simple dir', function(done_it) {
-
       find(fs_root).then(function(files) {
-
         var rel = relList(fs_root, files);
-
         expect(rel).to.deep.equal(['a', 'b', 'c/d', 'c/e', 'f/g', 'f/h']);
-
       }).done(done_it);
 
     });
 
     it('should filter on a RegExp', function(done_it) {
-
       find(fs_root, /[ae]$/).then(function(files) {
-
         var rel = relList(fs_root, files);
-
         expect(rel).to.deep.equal(['a', 'c/e']);
-
       }).done(done_it);
 
     });
