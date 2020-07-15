b3bba501b3283420343b4ca01024866bca0b9f17
Andy Armstrong <andy@hexten.net>
2020-07-15 13:23:08 +0100

    Minor tidying.

--- bin/manifest.js
+++ bin/manifest.js
@@ -1,17 +1,23 @@
 "use strict";
 
-var manifest = require('../lib/manifest.js');
-var fs = require('fs');
-var path = require('path');
+const manifest = require("../lib/manifest.js");
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+const path = require("path");
 
-var WEBROOT = 'www';
+const WEBROOT = "www";
 
-var args = process.argv.slice(2);
-var artroot = WEBROOT + '/art';
+const args = process.argv.slice(2);
+let artroot = WEBROOT + "/art";
 if (args.length) artroot = args[0];
 
-manifest(artroot, /\.(?:png|jpeg|jpg)$/i, '/art').then(function(mani) {
-  var out = fs.createWriteStream(path.join(artroot, 'manifest.json'));
-  out.write(JSON.stringify(mani, null, 2));
-  out.end();
-});
+(async () => {
+  try {
+    const mani = await manifest(artroot, /\.(?:png|jpeg|jpg)$/i, "/art");
+    const name = path.join(artroot, "manifest.json");
+    await fs.writeFileAsync(name, JSON.stringify(mani, null, 2));
+  } catch (e) {
+    console.error(e);
+    process.exit(1);
+  }
+})();
